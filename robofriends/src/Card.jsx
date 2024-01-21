8e486455b42101eac405a7a5c9e15349f7140330
valentinciolofan <valentincatalin02@gmail.com>
2024-01-21 21:05:12 +0200

    robofriends

---
+++ robofriends/src/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import 'tachyons'
+import CardList from './CardList';
+
+const Card = ({name, email, id}) => {
+    return (
+        <div className="tc bg-light-green dib br3 ma2 grow bw2 shadow-5">
+            <img alt='robot' src={`https://robohash.org/${id}?200x200}`}/>
+            <div className="">
+                <h2>{name}</h2>
+                <p>{email}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
---
+++ robofriends/src/robots.js
@@ -0,0 +1,52 @@
+export const robots = [
+{ id: 1,
+  name: 'Optimus Prime', 
+  username: 'OP', 
+  email: 'prime@gmail.com' 
+},
+{ id: 2,
+  name: 'Megatron', 
+  username: 'Meg', 
+  email: 'megatron@gmail.com' 
+},
+{ id: 3, 
+  name: 'Bumblebee', 
+  username: 'BB', 
+  email: 'bumblebee@gmail.com' 
+},
+{ id: 4, 
+  name: 'Starscream', 
+  username: 'Stars', 
+  email: 'starscream@gmail.com' 
+},
+{ id: 5, 
+  name: 'Soundwave', 
+  username: 'Sound', 
+  email: 'soundwave@gmail.com' 
+},
+{ id: 6, 
+  name: 'Grimlock', 
+  username: 'Grim', 
+  email: 'grimlock@gmail.com' 
+},
+{ id: 7, 
+  name: 'Shockwave', 
+  username: 'Shock', 
+  email: 'shockwave@gmail.com' 
+},
+{ id: 8, 
+  name: 'Ratchet', 
+  username: 'Rat', 
+  email: 'ratchet@gmail.com' 
+},
+{ id: 9, 
+  name: 'Ironhide', 
+  username: 'Iron', 
+  email: 'ironhide@gmail.com' 
+},
+{ id: 10, 
+  name: 'Wheeljack', 
+  username: 'Wheel', 
+  email: 'wheeljack@gmail.com' 
+}
+];
